85c266abf35e038b68ba92326b7703337e4f993e
IVTDubna2020 <74402941+IVTDubna2020@users.noreply.github.com>
2020-12-18 14:08:23 +0300

    Add files via upload
    Контрольная №3

---
+++ GromowAnton/Control work 3/Var5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+float Fr(float,float);
+int main()
+{
+   for(float x=0; x<=2; x+=0.25)
+    {        
+        for (float y=-0.5; y<=0.5;y+=0.1)
+	  if(
+	     printf("%.3f \n",Fr(x, y));
+        printf("\n");
+    }
+}
+
+float Fr(float x,float y)
+{
+  return ((exp(x)+exp(x*-1))/(exp(y)-exp(y*-1)))*(asin((x+y)/2)*(180/M_PI));
+}
