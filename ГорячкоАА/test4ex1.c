73efa8c9185cfd0b06e9b665b2e7a0a162b3db51
IVTDubna2020 <74402941+IVTDubna2020@users.noreply.github.com>
2021-04-03 12:05:36 +0300

    changed wronge file

--- ГорячкоАА/test4ex1.c
+++ ГорячкоАА/test4ex1.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
 #include <time.h>
-#define SIZE 10
+#define S 20
 
-int main ()
+int main()
 {
-	char X[SIZE];
-	srand(time(NULL));
-	for(int i=0; i<SIZE; i++)
-	{
-     X[i] = 0 + rand() % 250;
-      printf("%c\t",X);
+    char a[S];
+    srand(time(NULL));
+    printf("зададим случайный массив: \n");
+    for(int i = 0; i < S; i++)
+    {
+        a[i] = 32 + rand()%127;
+        printf(" %c\t",a[i]);
     }
-	return 0;
-} 
+     printf("\n заменим каждую строчную латинскую букву \n ");
+   
+    printf("\n \n количество перемен: %d \n", change (a, S));
+    
+    return 0;
+}
 
+int change (char b[], int size)
+{
+    int count = 0;
+    for(int i = 0; i < size; i++)
+    {
+        if (b[i]>=97 && b[i]<=122)
+        {
+            b[i]=36;
+            count ++;
+        }
+        printf (" %c \t ",b[i]);
+    }
+    return count;
+}
